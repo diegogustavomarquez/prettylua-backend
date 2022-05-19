e52cd6a2e2a5263953f3f45c90708355e6818b9a
diegogustavomarquez <getcoffee.ar@gmail.com>
2022-05-19 13:57:12 -0300

    make it better

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import Server from './classes/server';
 import mongoose from 'mongoose';
 
 import bodyParser from 'body-parser';
@@ -9,48 +8,33 @@ import postRoutes from './routes/post';
 import emailRoutes from './routes/email';
 
 import cors from 'cors';
+
+const express = require('express');
+const app = express();
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const urlDataBase = process.env.DATABASE || 'mongodb://mongodev:secret@localhost:27017/prettyluadb?authSource=admin';
 
-const server = new Server();
-
-console.log('urlDataBase' + urlDataBase);
-
- /*
-server.app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-})*/
+console.log('port: ' + port);
+console.log('urlDataBase: ' + urlDataBase);
 
 //habilitar cors
-server.app.use( cors({origin:true, credentials:true}));
+app.use( cors({origin:true, credentials:true}));
 
 
 // Body parser
-server.app.use( bodyParser.urlencoded({ extended: true }));
-server.app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.json() );
 
 
 // FileUpload
-server.app.use( fileUpload({ useTempFiles: true }) );
+app.use( fileUpload({ useTempFiles: true }) );
 
-  
 // Rutas de mi app
-server.app.use('/user', userRoutes );
-server.app.use('/posts', postRoutes );
-server.app.use('/email', emailRoutes );
-
-
-// Conectar DB
-/* mongoose.connect('mongodb://mongodev:secret@localhost:27017/prettyluadb?authSource=admin', ( err ) => {
-   if ( err ) throw err;
-   console.log('Base de datos ONLINE');
-}); */
+app.use('/user', userRoutes );
+app.use('/posts', postRoutes );
+app.use('/email', emailRoutes );
 
 mongoose.default.connect(urlDataBase, (err) => {
     if (err)
@@ -58,21 +42,6 @@ mongoose.default.connect(urlDataBase, (err) => {
     console.log('Base de datos ONLINE');
 });
 
-// mongoose.connect('mongodb://localhost:27017', {
-//     dbName: 'prettyluadb',
-//     auth: {
-//       password: 'secret',
-//       username: 'mongodev',
-//     },
-//   }, ( err ) => {
-//    if ( err ) throw err;
-//    console.log('Base de datos ONLINE');
-// });
-
-server.app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
-// Levantar express
-// server.start( () => {
-//     console.log(`Servidor corriendo en puerto ${ server.port }`);
-// });
