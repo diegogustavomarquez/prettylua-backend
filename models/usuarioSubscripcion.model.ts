21133683f9bf1ce80982b7cfeadda924325a05d3
diegogustavomarquez <getcoffee.ar@gmail.com>
2022-06-09 11:34:36 -0300

    se agrega el findByUserId para obtener subscripcion

--- models/usuarioSubscripcion.model.ts
+++ models/usuarioSubscripcion.model.ts
@@ -7,6 +7,42 @@ const usuarioSubscripcionSchema = new Schema({
         type: String,
         required: [ true, 'El nombre es necesario' ]
     },
+    cuitcuil: {
+        type: String,
+        required: [ true, 'El cuit/cuil es necesario' ]
+    },
+    fechaInicio: {
+        type: String,
+        required: [ true, 'El fecha inicio es necesario' ]
+    },
+    responsable: {
+        type: String,
+        required: [ true, 'El Responsable es necesario' ]
+    },
+    direccionFisica: {
+        type: String,
+        required: [ true, 'La direccion fisica es necesario' ]
+    },
+    titularTarjeta: {
+        type: String,
+        required: [ true, 'El Titular es necesario' ]
+    },
+    prefijo: {
+        type: String,
+        required: [ true, 'La tarjeta es necesaria es necesaria' ]
+    },
+    subfijo: {
+        type: String,
+        required: [ true, 'La tarjeta es necesaria es necesaria' ]
+    },
+    caducidad: {
+        type: String,
+        required: [ true, 'El caducidad es necesario' ]
+    },
+    cvc: {
+        type: String,
+        required: [ true, 'El cvc es necesario' ]
+    },
     userId: {
         type: String,
         unique: true,
@@ -16,6 +52,15 @@ const usuarioSubscripcionSchema = new Schema({
 
 interface IUsuarioSubscripcion extends Document {
     nombre: string;
+    cuitcuil: string;
+    fechaInicio: string;
+    responsable: string;
+    direccionFisica: string;
+    titularTarjeta: string;
+    prefijo: string;
+    subfijo: string;
+    caducidad: String;
+    cvc: string;
     userId : String;
 }
 
